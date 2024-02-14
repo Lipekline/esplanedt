f2d10ddb03868f2d6177c41d7f0154bf63c9e4c0
Luís Felipe Borges Silva <luis.silva@nanoincub.com.br>
2024-02-14 18:31:41 -0300

    Ajusta exibição da tela Home

--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,3 +1,20 @@
 import styled from 'styled-components/native';
+import {Colors} from '../../styles/colors';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  padding: 0px 20px;
+  background-color: ${Colors.white};
+`;
+
+export const HeaderContainer = styled.View`
+  align-items: center;
+`;
+
+export const BodyContainer = styled.View``;
+
+export const Title = styled.Text`
+  color: ${Colors.secondary};
+  font-weight: bold;
+  font-size: 24px;
+  text-align: center;
+`;
